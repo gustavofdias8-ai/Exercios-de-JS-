b1223a5249bec54b9f745feeca18037983b73edf
faccinadiasgustavo10-célula <faccinadiasgustavo10@gmail.com>
2026-05-29 17:34:03 -0300

    Exercícios javascript

---
+++ Exercícios de Fixação 👍/FixaçãoEX.js
@@ -0,0 +1,54 @@
+// let nome=" Gustavo ";
+// let idade=" 18 ";
+// let profissao=" desempregado ";
+// let cidade=" São Caetano Sul ";
+// console.log("Meu nome é"+nome+",tenho"+idade+"anos,trabalho como "+profissao+" e moro em "+cidade)
+
+
+// //exercios 2
+// let produto=" tv ";
+// let preco=" 5000 ";
+// let quantidade=" 3 "
+//     console.log("comprei"+quantidade+"unidades de"+produto+"por R$"+preco)
+
+
+// //exercios 3 
+// let valor1= 10 
+// let valor2= 20 
+// console.log("Soma:"+(valor1+valor2)+" Subratação:"+(valor1-valor2)+" Multiplicação:"+(valor1*valor2)+" Divisão:"+(valor1/valor2));
+
+//exercios 4
+// let tex="Olá";
+// let numero=10;
+// let ativo="verdade";
+// let vazio="Nada";
+// let lista=[1, 2, 3]
+
+// console.log(tex+" - "+ typeof tex)
+// console.log(numero+" - "+ typeof numero)
+// console.log(ativo+" - "+ typeof ativo)
+// console.log(vazio+" - "+ typeof vazio)
+// console.log(lista+" - "+ typeof lista)
+
+//execío 5
+// let numero="100"
+// let convertido=Number(numero)
+//     console.log(convertido)
+//     console.log(typeof numero)
+
+//Desafio
+// let nome=prompt("Digite o seu nome");
+// let idade=prompt("Digite a sua idade")
+// let altura=prompt("Digite a sua altura")   
+// let peso=prompt("Digite o seu peso")
+// let cidade=prompt("Digite a sua cidade")
+// let profissao=prompt("Digite a sua profissao")
+
+//     alert("Cadastro do usuário!")
+//         console.log("Nome:"+nome+
+//             " Idade:"+idade+
+//             " Altura:"+altura+
+//             " Peso:"+peso+
+//             " Cidade:"+cidade+
+//             " Profissão:"+profissao);        
+    
